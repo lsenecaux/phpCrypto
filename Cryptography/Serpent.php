b9c2afecde42b592ce7da1b4aef8a3cb07ce2c2b
lsenecaux <lsenecaux@users.noreply.github.com>
2016-10-10 14:51:03 +0200

    Update Serpent.php

--- Cryptography/Serpent.php
+++ Cryptography/Serpent.php
@@ -8,10 +8,6 @@ namespace Cryptography;
  */
 final class Serpent extends SymmetricAlgorithm
 {
-    const SERPENT_128 = 16;
-    const SERPENT_192 = 24;
-    const SERPENT_256 = 32;
-    
     /**
      * Creates a cryptographic object that is used to perform the symmetric algorithm
      * @return \Cryptography\Serpent
@@ -19,41 +15,17 @@ final class Serpent extends SymmetricAlgorithm
     public static function Create()
     {
         $object = new Serpent();
-        $object->KeySize = self::SERPENT_128;
+        $object->_properties['LegalKeySize'] = array(
+            parent::KEY_SIZE_128,
+            parent::KEY_SIZE_192,
+            parent::KEY_SIZE_256
+        );
+        $object->KeySize = parent::KEY_SIZE_128;
         
         // Serpent uses blocks of size 16 (128 bits)
         $object->_cipherAlg = MCRYPT_SERPENT;
         $object->BlockSize = 16;
         
         return $object;
-    }
-    
-    /**
-     * @see \Cryptography\SymmetricAlgorithm::GenerateIV()
-     */
-    public function GenerateIV()
-    {
-        if ($this->BlockSize != 16)
-            throw new \Exception(sprintf('%s : Block of size %d not supported by this algorithm. Only blocks of size 16 are supported', __METHOD__, $this->BlockSize));
-    
-        parent::GenerateIV();
-    }
-    
-    /**
-     * @see \Cryptography\SymmetricAlgorithm::GenerateKey()
-     */
-    public function GenerateKey()
-    {
-        switch ($this->KeySize)
-        {
-            case self::SERPENT_128:
-            case self::SERPENT_192:
-            case self::SERPENT_256:
-                parent::GenerateKey();
-                break;
-    
-            default:
-                throw new \Exception(sprintf('%s : Key of size %d not supported by this algorithm. Only keys of sizes 16, 24 or 32 are supported', __METHOD__, $this->KeySize));
-        }
-    }
+    }   
 }
