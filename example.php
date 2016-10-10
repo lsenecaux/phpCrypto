ff45463faeba22bb1aa7923c288a4565d7021e26
lsenecaux <lsenecaux@users.noreply.github.com>
2016-10-10 17:09:54 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,5 +1,10 @@
 <?php
 
+require_once 'Cryptography\SymmetricAlgorithm.php';
+require_once 'Cryptography\AES.php';
+require_once 'Cryptography\CipherMode.php';
+require_once 'Cryptography\PaddingMode.php';
+
 $object = \Cryptography\AES::Create();
 $object->KeySize = \Cryptography\SymmetricAlgorithm::KEY_SIZE_256;
 $object->Mode = \Cryptography\CipherMode::CBC;
