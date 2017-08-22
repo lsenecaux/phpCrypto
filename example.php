f55a77754ec955e2f80846e5f8742d98cc5ff640
lsenecaux <lsenecaux@users.noreply.github.com>
2017-08-22 20:18:16 +0200

    Update example.php

--- example.php
+++ example.php
@@ -5,6 +5,10 @@ require_once 'Cryptography/AES.php';
 require_once 'Cryptography/CipherMode.php';
 require_once 'Cryptography/PaddingMode.php';
 
+require_once 'Cryptography/AsymmetricAlgorithm.php'
+require_once 'Cryptography/RSA.php'
+
+// Symmetric
 $object = \Cryptography\AES::Create();
 $object->KeySize = \Cryptography\SymmetricAlgorithm::KEY_SIZE_256;
 $object->Mode = \Cryptography\CipherMode::CBC;
@@ -13,5 +17,13 @@ $object->GenerateKey();
 $object->GenerateIV();
 $cipher = $object->Encrypt('Hello, world!');
 $plaintext = $object->Decrypt($cipher);
+
+// Asymmetric
+$object = \Cryptography\RSA::Create(2048);
+$data = 'Hello, world!';
+$sign = $object->Sign($data);
+$verify = $object->Verify($data, $sign);
+$encData = $object->Encrypt($data);
+$plainData = $object->Decrypt($encData);
 
 ?>
