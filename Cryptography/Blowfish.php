d2e313aa0dab469f3736bb5b5626e17c1d465e8b
lsenecaux <lsenecaux@users.noreply.github.com>
2017-11-17 16:31:00 +0100

    KeySize can be passed as a parameter

--- Cryptography/Blowfish.php
+++ Cryptography/Blowfish.php
@@ -12,11 +12,11 @@ final class Blowfish extends SymmetricAlgorithm
      * Creates a cryptographic object that is used to perform the symmetric algorithm
      * @return \Cryptography\Blowfish
      */
-    public static function Create()
+    public static function Create($KeySize = parent::KEY_SIZE_128)
     {
         $object = new Blowfish();
         $object->_properties['LegalKeySizes'] = array_map(function($f) { return $f; }, range(4,56));
-        $object->KeySize = parent::KEY_SIZE_128;
+        $object->KeySize = $KeySize;
 
         // Blowfish uses blocks of size 8 (64 bits)
         $object->_cipherAlg = MCRYPT_BLOWFISH;
