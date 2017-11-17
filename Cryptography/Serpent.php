8588663e672f4807d37f3dff99eae0718150218f
lsenecaux <lsenecaux@users.noreply.github.com>
2017-11-17 16:32:01 +0100

    KeySize can be passed as a parameter

--- Cryptography/Serpent.php
+++ Cryptography/Serpent.php
@@ -12,7 +12,7 @@ final class Serpent extends SymmetricAlgorithm
      * Creates a cryptographic object that is used to perform the symmetric algorithm
      * @return \Cryptography\Serpent
      */
-    public static function Create()
+    public static function Create($KeySize = parent::KEY_SIZE_128)
     {
         $object = new Serpent();
         $object->_properties['LegalKeySizes'] = array(
@@ -20,7 +20,7 @@ final class Serpent extends SymmetricAlgorithm
             parent::KEY_SIZE_192,
             parent::KEY_SIZE_256
         );
-        $object->KeySize = parent::KEY_SIZE_128;
+        $object->KeySize = $KeySize;
         
         // Serpent uses blocks of size 16 (128 bits)
         $object->_cipherAlg = MCRYPT_SERPENT;
